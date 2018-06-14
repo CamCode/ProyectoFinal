fe963b559bc7a3da8ce6cd2f235dcebf269a9aa6
EDUAR AUGUSTO OJEDA JIMENEZ <eduar.ojeda2220@correo.policia.gov.co>
2018-06-14 18:35:49 -0500

    error

--- src/app/routing.ts
+++ src/app/routing.ts
@@ -3,8 +3,8 @@ import { HomeComponent } from './globals/pages/home/home.component';
 import { CategoriaComponent } from './globals/pages/categoria/categoria.component';
 
 const routerM:Routes = [
-    {path:'', component:HomeComponent},
-    {path:'categoria', component:CategoriaComponent}// path vacio para apuntar a home
+    {path:'', component:HomeComponent},// path vacio para apuntar a home
+    {path:'categora', component:CategoriaComponent}
 ];
 export const appRouter = RouterModule.forRoot(routerM);
 
