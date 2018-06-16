d680dda2d8bfda3c33bdf93e935f9b65dd84685c
EDUAR AUGUSTO OJEDA JIMENEZ <eduar.ojeda2220@correo.policia.gov.co>
2018-06-15 20:56:07 -0500

    Error eventemitter

--- src/app/globals/slider/sliderproducto/producto-item/producto-item.component.ts
+++ src/app/globals/slider/sliderproducto/producto-item/producto-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';//generar input aca
 import { Producto } from '../../../../models/producto';
+import { checkAndUpdatePureExpressionDynamic } from '@angular/core/src/view/pure_expression';
+import { EventEmitter } from 'events';
 
 @Component({
   selector: 'app-producto-item',
@@ -8,13 +10,15 @@ import { Producto } from '../../../../models/producto';
 })
 export class ProductoItemComponent implements OnInit {
   @Input() producto: Producto; //para llaamrlo acà 
+  @Output() anexar : EventEmitter <Producto>();
 
   AgregarItem() {
     console.log(`Agregado ${this.producto.id}:${this.producto.nombre}`);
 
     //algun proceso aca...
 
-    //disparar el nuevo evento
+    //disparar el nuevo evento`
+   
   }
 
   constructor() {
--- src/app/globals/slider/sliderproducto/sliderproducto.component.ts
+++ src/app/globals/slider/sliderproducto/sliderproducto.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Producto } from '../../../models/producto';
 
 @Component({
@@ -7,6 +7,11 @@ import { Producto } from '../../../models/producto';
   styleUrls: ['./sliderproducto.component.scss']
 })
 export class SliderproductoComponent implements OnInit {
+  @Output() anexar = new EventEmitter <Producto>();
+  anexar(producto:Producto){
+
+  }
+
   productos: Producto[];//Ctrl + space para sugerencias productos es una lista de Producto
   constructor() {
     this.productos = new Array <Producto>();// nuevo arreglo <Template> de (poisiciones)
@@ -21,10 +26,11 @@ export class SliderproductoComponent implements OnInit {
       p.image = `https://loremflickr.com/320/240/cats`;
 
       this.productos.push(p);
-
     }
-
    }
+  
+  
+
 
   ngOnInit() {
   }
